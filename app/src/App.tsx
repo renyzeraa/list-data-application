5f1bef0f6662c8977417d912a55571cbbb3d2843
Renan Silva <renansilvaytB@gmail.com>
2024-02-24 17:16:26 -0300

    install postcss and autoprefixer

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,19 @@
+import { Plus } from 'lucide-react'
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="py-10 space-y-8">
+      <div>
+        header 
+        tabs
+      </div>
+      <main className="max-w-6xl space-y-5 nx-auto">
+        <h1 className="text-xl font-bold">Tags</h1>
+        <button className='inline-flex items-center gap-1.5 '>
+          <Plus className='size-3'></Plus>
+          Create new
+        </button>
+      </main>
+  </div>  
   )
 }
