9ca710658f151209aaac42339e103e3769dc6aab
Renan Silva <renansilvaytB@gmail.com>
2024-02-26 18:37:26 -0300

    feat Tags response fetch

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,9 +4,43 @@ import { Tabs } from './components/tabs'
 import { Button } from './components/ui/button'
 import { Control, Input } from './components/ui/input'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
-import { Pagination } from './components/pagination.tsx'
+// import { Pagination } from './components/pagination'
+import { useQuery } from '@tanstack/react-query'
+
+export interface TagResponse{
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  slug: string
+  amountOfVideos: number
+  id: string
+}
+
 
 export function App() {
+
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags'],
+    queryFn: async () => {
+       const response = await fetch('http://localhost:3333/tags?_page=1&_per_page=10')
+       const data = await response.json()
+       console.log(data)
+       return data
+    }
+  })
+
+  if (isLoading) {
+    return null
+  }
+
   return (
     <div className="py-10 space-y-8">
       <div>
@@ -42,18 +76,18 @@ export function App() {
           </TableHeader>
           <TableBody>
             {
-              Array.from({length: 10}).map((_,i) => {
+              tagsResponse?.data.map((tag) => {
                   return (
-                    <TableRow key={i}>
+                    <TableRow key={tag.id}>
                     <TableCell></TableCell>
                     <TableCell>
                       <div className='flex flex-col gap-0.5'>
-                        <span className='font-medium'>React</span>
-                        <span className='text-xs text-zinc'>01230102-POM390XDN-A3MFS4F82-09123K</span>
+                        <span className='font-medium'>{tag.title}</span>
+                        <span className='text-xs text-zinc'>{tag.id}</span>
                       </div>
                     </TableCell>
                     <TableCell className='text-zinc-300'>
-                      13 video(s)
+                      {tag.amountOfVideos} video(s)
                     </TableCell>
                     <TableCell className='text-right'>
                       <Button size='icon'>
@@ -66,7 +100,7 @@ export function App() {
             }
           </TableBody>
         </Table>
-        {/* <Pagination items={3} page={1} pages={2}/> */}
+        {/* <Pagination/> */}
       </main>
   </div>  
   )
