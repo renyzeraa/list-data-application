048c665c49e44ad8829b4f410565f75f46ea52b2
Renan Silva <renansilvaytB@gmail.com>
2024-03-05 18:34:11 -0300

    feat create new tag and refresh list data

--- app/src/components/create-tag-form.tsx
+++ app/src/components/create-tag-form.tsx
@@ -4,6 +4,7 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button } from "./ui/button";
 import * as Dialog from '@radix-ui/react-dialog'
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const createTagSchema = z.object({
   title: z.string().min(3, { message: 'Minimum 3 characters.' })
@@ -21,26 +22,40 @@ function getSlugFromString(input: string): string {
 type CreateTagSchema = z.infer<typeof createTagSchema>
 
 export function CreateTagForm() {
+  const queryClient = useQueryClient()
+
   // Register ira registrar cada input, falar que o input pertence a um formulário
-  // formState 
+  // formState devolve o estado do submitting
   const { register, handleSubmit, watch, formState } = useForm<CreateTagSchema>({
     resolver: zodResolver(createTagSchema),
   })
 
   const slug = watch('title') ? getSlugFromString(watch('title')) : ''
 
+  const { mutateAsync } = useMutation({
+    mutationFn: async ({ title }: CreateTagSchema) => {
+      await fetch('http://localhost:3333/tags', {
+        method: 'POST',
+        body: JSON.stringify({
+          title,
+          slug,
+          amountOfVideos: 0
+        }),
+      })
+    },
+    /**
+     * Basicamente ira criar a tag e avisar a query que ela deve recarregar, pois criei uma nova
+     */
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['get-tags']
+      })
+    },
+  })
+
   async function createTag({ title }: CreateTagSchema) {
-    await fetch('http://localhost:3333/tags', {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        slug,
-        amountOfVideos: 0
-      }),
-    })
+    await mutateAsync({ title })
   }
-
-
 
   return (
     <form onSubmit={handleSubmit(createTag)} className="w-full space-y-6">
